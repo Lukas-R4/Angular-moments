48af50155af1c9997823103ee21a149854423de1
Lucas Rodrigues <lucas93.rodrigues@gmail.com>
2024-10-01 07:33:56 -0300

    feat: Implementando o ReactiveForms

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,16 +1,43 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+  styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent implements OnInit {
-  @Input() btnText!: string;
+  @Input() public btnText!: string;
+  public momentForm!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      image: new FormControl(''),
+    });
   }
 
+  get title(): FormControl {
+    return this.momentForm.get('title') as FormControl;
+  }
+
+  get description(): FormControl {
+    return this.momentForm.get('description') as FormControl;
+  }
+
+  get image(): FormControl {
+    return this.momentForm.get('image') as FormControl;
+  }
+
+  submit(): void {
+    if (this.momentForm.invalid) {
+      return;
+    }
+
+    console.log('submit');
+  }
 }
