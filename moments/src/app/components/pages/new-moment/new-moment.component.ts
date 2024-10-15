bd9eb7506c9935bdd1a1ec41e177c0a7b896101e
Lucas Rodrigues <lucas93.rodrigues@gmail.com>
2024-10-15 06:50:29 -0300

    feat: Configurar Envio de dados para o back-end

--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -8,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class NewMomentComponent implements OnInit {
   public btnText = 'Compartilhar!';
 
-  constructor() { }
+  constructor(private _momentService: MomentService) { }
 
   ngOnInit(): void {
+  }
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    await this._momentService.createMoment(formData).subscribe();
+
+    // TODO: Exibir mensagem de sucesso;
+    // TODO: Redirecionar para a página de momentos;
   }
 
 }
